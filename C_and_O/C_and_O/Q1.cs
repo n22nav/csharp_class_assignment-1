77db17314d7ad78c5b6b24c32d7220e602b8ac94
unknown <socgendot15@iiht.tech>
2021-10-22 00:31:48 +0530

    commit_2

--- C_and_O/C_and_O/Q1.cs
+++ C_and_O/C_and_O/Q1.cs
@@ -8,6 +8,25 @@ namespace C_and_O
 {
     class Q1
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter total credit amount");
+            double credit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the monthly payment amount");
+            double emi = double.Parse(Console.ReadLine());
+            int i = 1;
+            double pay = 0;
 
+
+            while (credit > emi)
+            {
+                pay += emi;
+                credit = 1.015 * credit - emi;
+                Console.WriteLine("Month {0}:\tBalance: {1}\tPayment: {2}", i, credit, pay);
+                i++;
+            }
+            Console.WriteLine("Month {0}:\tBalance: {1}\tPayment: {2}", i, credit, arg2:pay);
+
+        }
     }
 }
